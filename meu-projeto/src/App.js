a2f1b24350668c6d6f7004c7ee87f2fe9a63771d
paivagpg77 <paivag171@gmail.com>
2026-01-18 19:11:04 -0300

    props

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,30 +1,15 @@
 import './App.css';
-import HelloWolrd  from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const name = 'Gabriel'
-  const Newname = name.toUpperCase()
-
-  function sum(a,b){
-    return a+b
-  }
-  function div(a,b){
-    return a/b
-  }
-
-  const url = "https://picsum.photos/300/200"
-  const url2 = "https://placehold.co/300x200?text=React"
-
+  const nome = "Maria"
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Meu nome maiusculo é {Newname}</p>
-      <p>Soma: {sum(1,3)}</p>
-      <p>Divisão: {div(4,2)}</p>
-      
-      <img src={url2} alt='Minha imagem'/>
-      <HelloWolrd/>
-     
+      <SayMyName nome="Gabriel"/>
+      <SayMyName nome={nome}/>
+      <Pessoa nome="Gabriel" idade="15 anos" profissao="Programador"  foto="https://randomuser.me/api/portraits/men/32.jpg
+      "/>
     </div>
   );
 }
---
+++ meu-projeto/src/components/SayMyName.js
@@ -0,0 +1,9 @@
+function SayMyName(props){
+    return(
+        <div>
+            <p>Fala aí {props.nome}, Suave?</p>
+        </div>
+    )
+}
+
+export default SayMyName
