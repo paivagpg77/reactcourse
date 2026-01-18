03e662a8d93118ab70f8d7972968b63bb9d77f9a
paivagpg77 <paivag171@gmail.com>
2026-01-18 18:44:15 -0300

    working with components

---
+++ meu-projeto/src/components/HelloWorld.js
@@ -0,0 +1,15 @@
+import Frase from "./Frase"
+
+function HelloWolrd(){
+
+    return(
+        <div>
+            <h1>Meu Primeiro componente</h1>
+            <Frase/>
+        </div>
+    )
+
+}
+
+
+export default HelloWolrd
