5fd1925801e96d60a40cbef4aa309f0c9efc2c89
paivagpg77 <paivag171@gmail.com>
2026-01-18 17:58:58 -0300

    first react setup file

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá Mundo</h1>
+      <p>Meu primeiro App</p>
+    </div>
+  );
+}
+
+export default App;
