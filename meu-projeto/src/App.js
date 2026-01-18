6d2cbf45c4205d3aed54b582872e89e12f2c91ff
paivagpg77 <paivag171@gmail.com>
2026-01-18 18:22:59 -0300

    dealing with programming logic

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -2,10 +2,30 @@
 import './App.css';
 
 function App() {
+  const name = 'Gabriel'
+  const Newname = name.toUpperCase()
+
+  function sum(a,b){
+    return a+b
+  }
+
+  function div(a,b){
+    return a/b
+  }
+
+
+  const url = "https://picsum.photos/300/200"
+  const url2 = "https://placehold.co/300x200?text=React"
+
   return (
     <div className="App">
-      <h1>Olá Mundo</h1>
-      <p>Meu primeiro App</p>
+      <h1>Olá React</h1>
+      <p>Meu nome maiusculo é {Newname}</p>
+      <p>Soma: {sum(1,3)}</p>
+      <p>Divisão: {div(4,2)}</p>
+      
+      <img src={url2} alt='Minha imagem'/>
+
     </div>
   );
 }
